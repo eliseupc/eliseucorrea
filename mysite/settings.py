4286cddcbc4d6c1ac9164d442ac8f73775737a72
Eliseu Corrêa <eliseupaulinocorrea@hotmail.com>
2021-04-11 20:49:15 -0300

    Adiciando App de Usuários

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps de Terceiros
+
+    # Meus apps
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# User Model
+
+AUTH_USER_MODEL = 'users.User'
