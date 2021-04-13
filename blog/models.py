5be4d25b99f4ad644be8df4aeabfc968d5a6cb13
Eliseu Corrêa <eliseupaulinocorrea@hotmail.com>
2021-04-12 21:43:19 -0300

    Adicionado o app Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from users.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     # Local
     path('', include('pages.urls', namespace='pages')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
